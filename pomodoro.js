805b1320d098d527f41afb82f75c7a9da37ffebf
Mi Mapuwa <mapuwa@protonmail.com>
2016-07-19 20:58:14 +0200

    basic func fixed

--- pomodoro.js
+++ pomodoro.js
@@ -1,11 +1,17 @@
 function rev(x) {
     setTimeout(function () {
         x--;
-        document.getElementById("clock").innerHTML = x;
+        var mins = Math.floor(x/60);
+        var secs = x % 60;
+        document.getElementById("clock").innerHTML = (mins > 9 ? mins : "0"+ mins) + ":" + (secs > 9 ? secs : "0"+ secs);
         if (x) rev(x);
+        else alert("The end");
     }, 1000);
 }
 document.getElementById("start").addEventListener("click", function () {
-    var x = 360
+    var x = 13;
+    var mins = Math.floor(x/60);
+    var secs = x % 60;
+    document.getElementById("clock").innerHTML = (mins > 9 ? mins : "0"+ mins) + ":" + (secs > 9 ? secs : "0"+ secs);
     rev(x);
 });
