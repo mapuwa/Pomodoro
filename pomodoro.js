0b2d2b1f9a1b656e058ddfd04686248e6f31511b
Mi Mapuwa <mapuwa@protonmail.com>
2016-07-20 14:31:24 +0200

    refactor and add short break

--- pomodoro.js
+++ pomodoro.js
@@ -1,17 +1,18 @@
-function rev(x) {
-    setTimeout(function () {
-        x--;
-        var mins = Math.floor(x/60);
-        var secs = x % 60;
-        document.getElementById("clock").innerHTML = (mins > 9 ? mins : "0"+ mins) + ":" + (secs > 9 ? secs : "0"+ secs);
-        if (x) rev(x);
-        else alert("The end");
-    }, 1000);
-}
-document.getElementById("start").addEventListener("click", function () {
-    var x = 13;
+
+function timeout(x) {
     var mins = Math.floor(x/60);
     var secs = x % 60;
     document.getElementById("clock").innerHTML = (mins > 9 ? mins : "0"+ mins) + ":" + (secs > 9 ? secs : "0"+ secs);
-    rev(x);
+    setTimeout(function () {
+        x--;
+        if (x) timeout(x);
+        else {
+	    	document.getElementById("clock").innerHTML = "00:00"
+		alert("Short break");
+		timeout(5*60);
+	}
+    }, 1000);
+}
+document.getElementById("start").addEventListener("click", function () {
+    timeout(13);
 });
